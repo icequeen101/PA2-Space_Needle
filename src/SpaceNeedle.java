b2a21cb7a6f29043f5d040127c3fbeed74d373b2
icequeen101 <43222443+icequeen101@users.noreply.github.com>
2022-06-18 09:31:10 -0700

    Initial commit

---
+++ src/SpaceNeedle.java
@@ -0,0 +1,17 @@
+// Liana Wu
+// 6.18.2022
+// CSE142
+// Take-home Assessment #2
+//
+// A program that produces as ASCII Art
+
+
+public class SpaceNeedle {
+    public static void main(String[] args) {
+        System.out.println();
+
+    }
+
+
+    
+}
